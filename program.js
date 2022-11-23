5148b46b7c37df1a04fb5f17f4a96b3f4f3b6007
ademskaljo <ademskaljo@gmail.com>
2022-11-23 20:49:53 +0100

    prvi comit

--- program.js
+++ program.js
@@ -1,3 +1,13 @@
 console.log("Hello World");
 console.log("Drudi Hello da se ne patimo");
 
+var number1 = 23;
+var number2 = 34;
+var rezultat = number1+number2
+console .log(rezultat);
+
+let temperature = 33;
+temperature = "Danas je bio divan i sunčan dan bez smoga..."
+console.log(temperature);
+
+
