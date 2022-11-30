04a116a84985a6f2a71c32dc96c53777aa892f6c
ademskaljo <ademskaljo@gmail.com>
2022-11-30 17:24:52 +0100

    commit

--- program.js
+++ program.js
@@ -10,4 +10,21 @@ let temperature = 33;
 temperature = "Danas je bio divan i sunčan dan bez smoga..."
 console.log(temperature);
 
+let number = 011;
+console.log(number);
+
+
+let number1 = 1;
+let number2 = 34;
+let suma = sum(number1 + number2);
+
+let rezultat = sum(23, 34);
+
+function sum(num1, num2){
+    var rez = num1+ num2;
+    return num1 +num2;
+}
+
+console.log(rez);
+
 
