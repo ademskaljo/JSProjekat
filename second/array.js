0841ae357dd26439bcc9cfe1f308c7cb51c37f69
ademskaljo <ademskaljo@gmail.com>
2022-12-09 17:38:20 +0100

    Destructuring directories and Files

---
+++ second/array.js
@@ -0,0 +1,13 @@
+let person = {
+    name: "Ahmed"
+    surname: "Orahović"
+    age: 39
+}
+console.log(person.name);
+console.log(person["age"]);
+
+//polja koja nisu imenovana nego su indeksirana
+//prvo polje u nizu je indicirano sa 0
+
+let clubs = ["Čelik", "Sarajevo", "Velež", 23, 123];
+console.log(clubs[0]);
